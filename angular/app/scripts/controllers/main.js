49aab6202ac2de29ceea3ead440067054f25c9ee
Jose Baena <jose@brandwatch.com>
2015-09-21 22:51:48 +0100

    Angular app - Example of angular app (client side) - Use of angular generator. Default by yeoman

---
+++ angular/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name videostoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the videostoreApp
+ */
+angular.module('videostoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
